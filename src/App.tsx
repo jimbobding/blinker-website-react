417dcade48a6184ffc86796b1737669ebc1c1124
jimbobding <jimmygrahamdavid@gmail.com>
2023-11-27 12:19:55 +0000

    added contact form

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import React from "react";
 
+import "./css/reset.css";
 import "./css/index.css";
 import "./css/nav.css";
+import "./css/about.css";
+import "./css/bookingForm.css";
 
-import Index from "./components";
 import Nav from "./components/Nav";
+import About from "./components/About";
+import BookingForm from "./components/BookingForm";
+import Index from "./components/Index";
 
 function App() {
   return (
     <>
       <Nav />
+      <About />
       <Index />
+      <BookingForm />
     </>
   );
 }
