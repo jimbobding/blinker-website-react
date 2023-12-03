3b5d617b96948fadee57479f45772f9912677b51
jimbobding <jimmygrahamdavid@gmail.com>
2023-12-03 13:46:10 +0000

    changed colour and some styles on navbar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,109 +1,13 @@
-// import { useState } from "react";
-
-// const Navbar = () => {
-//   const scrollToSection = (sectionId: string) => {
-//     const element = document.getElementById(sectionId);
-//     if (element) {
-//       element.scrollIntoView({
-//         behavior: "smooth",
-//         block: "start",
-//       });
-//     }
-//   };
-
-//   return (
-//     <nav>
-//       <div className="nav-container">
-//         <img
-//           src="images/blinker-raspberry-black.png"
-//           alt=""
-//           className="nav-img"
-//         />
-//         <div className="menu">
-//           <span></span>
-//           <span></span>
-//           <span></span>
-//         </div>
-//         <ul>
-//           <li className="nav-li">
-//             <a
-//               href="#about"
-//               className="nav-link"
-//               onClick={() => scrollToSection("about")}
-//             >
-//               About
-//             </a>
-//           </li>
-
-//           <li className="nav-li">
-//             <a
-//               href="#contact"
-//               className="nav-link"
-//               onClick={() => scrollToSection("contact")}
-//             >
-//               Contact
-//             </a>
-//           </li>
-
-//           <li className="nav-li">
-//             <a
-//               href="#menu"
-//               className="nav-link"
-//               onClick={() => scrollToSection("menu")}
-//             >
-//               Menu
-//             </a>
-//           </li>
-
-//           <li className="nav-li">
-//             <a
-//               href="#merch"
-//               className="nav-link"
-//               onClick={() => scrollToSection("merch")}
-//             >
-//               Merch
-//             </a>
-//           </li>
-
-//           <li className="nav-li">
-//             <a
-//               href="#opening"
-//               className="nav-link"
-//               onClick={() => scrollToSection("opening")}
-//             >
-//               Opening
-//             </a>
-//           </li>
-//         </ul>
-//       </div>
-//     </nav>
-//   );
-// };
-
-// export default Navbar;
-
 import { useState } from "react";
+
+import { Link as ScrollLink } from "react-scroll";
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
-  const scrollToSection = (sectionId: string) => {
-    const element = document.getElementById(sectionId);
-    if (element) {
-      element.scrollIntoView({
-        behavior: "smooth",
-        block: "start",
-      });
-    }
-  };
 
   return (
     <nav>
       <div className="nav-container">
-        <img
-          src="images/blinker-raspberry-black.png"
-          alt=""
-          className="nav-img"
-        />
         <div className="menu" onClick={() => setMenuOpen(!menuOpen)}>
           {" "}
           <span></span>
@@ -112,24 +16,54 @@ const Navbar = () => {
         </div>
         <ul className={menuOpen ? "open" : ""}>
           <li className="nav-li">
-            <a href="#about" onClick={() => scrollToSection("about")}>
+            <ScrollLink
+              to="about"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+              onClick={() => setMenuOpen(false)}
+            >
               About
-            </a>
+            </ScrollLink>
           </li>
 
           <li className="nav-li">
-            <a href="#contact">Contact</a>
+            <ScrollLink
+              to="menu"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+              onClick={() => setMenuOpen(false)}
+            >
+              Menu
+            </ScrollLink>
           </li>
-
           <li className="nav-li">
-            <a href="#menu">Menu</a>
+            <ScrollLink
+              to="contact"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+              onClick={() => setMenuOpen(false)}
+            >
+              Contact
+            </ScrollLink>
           </li>
           <li className="nav-li">
-            <a href="#merch">Merch</a>
+            <ScrollLink
+              to="merch"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+              onClick={() => setMenuOpen(false)}
+            >
+              Merch
+            </ScrollLink>
           </li>
-          {/* <li className="nav-li">
-            <a href="#opening">Opening</a>
-          </li> */}
         </ul>
       </div>
     </nav>
