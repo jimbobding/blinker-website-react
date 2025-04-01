a8257a1c94c68953e38daa57ad4f762e624a91fb
Jimmy David <55886146+jimbobding@users.noreply.github.com>
2025-04-01 15:50:42 +0100

    Update Menu.tsx

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -39,7 +39,7 @@ const Menu = () => {
             <h2>Menu</h2>
 
             <a
-              href="menus/25.2.25_Blinker_Menu_March25.pdf"
+              href="menus/30.5.25_Blinker_Menu_April25.pdf"
               target="_blank"
               type="button"
               className="section-btn "
