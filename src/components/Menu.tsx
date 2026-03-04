71b4197f20a57e91baf36df0406f111f913cad8f
Jimmy David <55886146+jimbobding@users.noreply.github.com>
2026-03-04 13:04:46 +0000

    Update menu link to new PDF file

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -39,7 +39,7 @@ const Menu = () => {
             <h2>Menu</h2>
 
             <a
-              href="menus/27.01.26_Blinker_Menu_March26.pdf"
+              href="menus/27.01.26_Blinker_Menu_March26 (1).pdf"
               target="_blank"
               type="button"
               className="section-btn "
