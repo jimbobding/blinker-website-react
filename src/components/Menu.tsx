08d367a226e5c14ae23dd4c0ab894f1694250ea7
Jimmy David <55886146+jimbobding@users.noreply.github.com>
2026-03-30 15:02:14 +0100

    Update Menu.tsx
    added april menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -39,7 +39,7 @@ const Menu = () => {
             <h2>Menu</h2>
 
             <a
-              href="menus/27.01.26_Blinker_Menu_March26 (1).pdf"
+              href="menus/24.03.26_Blinker_Menu_April26.pdf"
               target="_blank"
               type="button"
               className="section-btn "
